b3a063e68e6badbd578905e3a9a944ca38b34dbb
shubhe25p <shubhpachchigar@gmail.com>
2023-12-11 00:06:35 -0800

    debug

--- main.c
+++ main.c
@@ -90,7 +90,7 @@ void processWordsfromFile(long int fsize)
 				count++;
                }
         }
-	# pragma omp parallel for
+	# pragma omp parallel for num_threads(1)
 	for(int i = 0; i < count; i++)
 	{
 		int flag = 0;
