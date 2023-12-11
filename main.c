2c77b1e304e797473f43fa223d8cff562b88d714
shubhe25p <shubhpachchigar@gmail.com>
2023-12-10 23:47:10 -0800

    debug

--- main.c
+++ main.c
@@ -95,6 +95,7 @@ void processWordsfromFile(long int fsize)
 	{
 		for(int j=0;j<=windex;j++)
 		{
+			printf("arr[%d] = %s with index %d\n", i, arr[i], windex);
 			if(strcmp(arr[i], word[j].wstring) == 0)
 			{
 				word[j].count++;
